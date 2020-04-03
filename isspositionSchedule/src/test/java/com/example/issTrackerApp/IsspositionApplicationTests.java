550fc49ab52b94a60bf0a806c28fe91af3b6457a
MLY <MLY@TUNLAP018.ESI-INTERNAL.esi-group.com>
2020-04-03 04:09:34 +0100

    dockerfile, and accessing data base for unit test

--- isspositionSchedule/src/test/java/com/example/issTrackerApp/IsspositionApplicationTests.java
+++ isspositionSchedule/src/test/java/com/example/issTrackerApp/IsspositionApplicationTests.java
@@ -2,12 +2,21 @@ package com.example.issTrackerApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
 @SpringBootTest
 class IsspositionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void addingNewPosition() throws Exception{
+		Class.forName("org.h2.Driver");
+        DBI dbi = new DBI("jdbc:h2:mem:ali");
+        handle = dbi.open();
+
 	}
 
 }
